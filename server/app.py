200842310e4204ea5f0450c059a43a5d9599687f
topher ludlow <topherludlow@protonmail.com>
2023-03-27 09:28:45 -0700

    commit

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
