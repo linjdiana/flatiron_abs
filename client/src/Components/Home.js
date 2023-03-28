89c969fee04e53c1c35c41617e1fbd1152af7d56
topher ludlow <topherludlow@protonmail.com>
2023-03-27 21:23:11 -0700

    blah

---
+++ client/src/Components/Home.js
@@ -0,0 +1,18 @@
+function Home(){
+   return (
+   <article>
+          <h1>What is Flat & Iron Abs Gym? </h1>
+          Flat & Iron Abs is a gym located in the Bay Area. This is created by three software engineers from the Flatiron School.  
+          <a
+            href="https://blog.logrocket.com/create-responsive-navbar-react-css/"
+            target="_blank"
+            rel="noreferrer"
+          >
+             Please watch a video tutorial.
+          </a>
+          <img src='https://github.com/linjdiana/flatiron_abs/blob/topher/images/javonn.png?raw=true' alt=''></img>
+        </article>
+   )
+}
+
+export default Home;
